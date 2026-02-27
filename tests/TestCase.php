9072d5d5299c6dcd416be1ba9329a51f236d2b58
alemian95 <43844752+alemian95@users.noreply.github.com>
2026-02-27 23:34:20 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace AleMian95\Datatable\Tests;
 
+use AleMian95\Datatable\DatatableServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use AleMian95\Datatable\DatatableServiceProvider;
 
 class TestCase extends Orchestra
 {
